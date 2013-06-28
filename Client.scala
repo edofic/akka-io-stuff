77512b2d80dc2023b597499a5270ee8ed2a12d28
Andraz Bajt <edofic@gmail.com>
2013-06-28 10:09:09 +0200

    manual ping-pong

--- Client.scala
+++ Client.scala
@@ -1,15 +1,15 @@
 import akka.actor._
+import akka.io.Udp.{Received, Bind, Bound, Send}
 import akka.io.{IO, Udp}
-import akka.io.Udp.{Send, SimpleSenderReady, SimpleSender}
 import akka.util.ByteString
 import java.net.InetSocketAddress
 
 class Client extends Actor with ActorBase {
   import context.system
-  IO(Udp) ! SimpleSender
+  IO(Udp) ! Bind(self, new InetSocketAddress(0))
 
   def receive = known {
-    case SimpleSenderReady => context.become(proxy(sender))
+    case Bound(adr) => context become proxy(sender)
   }
 
   val Msg = "(\\d+) +(.+)".r
@@ -19,6 +19,7 @@ class Client extends Actor with ActorBase {
       val adr = new InetSocketAddress(portText.toInt)
       val data = ByteString(msgText)
       client ! Send(data, adr)
+    case Received(bytes, _) => println("\ngot response: " + bytes.utf8String)
   }
 }
 
--- Server.scala
+++ Server.scala
@@ -1,32 +1,44 @@
 import akka.actor._
+import akka.io.Udp.Bound
+import akka.io.Udp.{Send, Received, Bind}
 import akka.io.{IO, Udp}
 import java.net.InetSocketAddress
 
 
-class Server extends Actor with ActorBase {
+class Server(verbose: Boolean) extends Actor with ActorBase {
   import context.system
 
-  IO(Udp) ! Udp.Bind(self, new InetSocketAddress(0))
+  IO(Udp) ! Bind(self, new InetSocketAddress(0))
 
   def receive = known {
-    case Udp.Bound(adr) =>
+    case Bound(adr) =>
       val port = adr.getPort
       println(s"Bound on $port")
-      context become listening
+      context become (if(verbose) listening else echo(sender))
   }
 
-  def listening = known {
-    case Udp.Received(data, sender) =>
-      println(s"""got "${data.utf8String}" from $sender""")
+  def listening = {
+    println("chatty mode")
+    known {
+      case Received(data, sender) =>
+        println(s"""got "${data.utf8String}" from $sender""")
+    }
   }
 
+  def echo(io: ActorRef) = {
+    println("echo mode")
+    known {
+      case Received(data, sender) => io ! Send(data, sender)
+    }
+  }
 }
 
 
 object Server {
   def main(args: Array[String]){
     val sys = ActorSystem()
-    val server = sys.actorOf(Props[Server])
+    val verbose = readLine("Verbose? [true/false] ").toBoolean
+    val server = sys.actorOf(Props(new Server(verbose)))
 
     readLine()
     sys.shutdown()
